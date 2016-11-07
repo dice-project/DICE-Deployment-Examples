46da382e76648de5fd651ff93dbf19cf42c52d00
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-11-07 11:46:10 +0100

    Add selective redirect
    Redirection script now allows us to redirect only blueprints that use
    specific version of DICE TOSCA library. This will come in handy when
    we need to create new release and we only want to update blueprints
    that use development branch at the moment.
    
    Change-Id: I5b027c8e885fd2f6f768544d2932badf1c1791e3

--- redirect.sh
+++ redirect.sh
@@ -2,7 +2,7 @@
 
 DEFAULT_BRANCH=tb-wip
 BASE="http://dice-project.github.io/DICE-Deployment-Cloudify"
-NEEDLE="$BASE/spec/([^/]+)/.*\\.yaml"
+NEEDLE="$BASE/spec/([^/]+)/__SRC__/plugin\\.yaml"
 REPLACEMENT="$BASE/spec/\\1/__BRANCH__/plugin.yaml"
 
 function usage ()
@@ -10,34 +10,40 @@ function usage ()
   cat <<EOF
 USAGE:
 
- $0 BLUEPRINT [BRANCH | TAG]
+ $0 BLUEPRINT [BRANCH | TAG] [BRANCH | TAG]
 
 This script can be used to update blueprint's import location, which can be
 quite usefull when developing new functionality or creating releases.
 
-If no branch or tag is specified, default branch 'tb-wip' is used.
+If no branch or tag is specified, default branch '$DEFAULT_BRANCH' is used.
 
 If special name 'all' is used in place of BLUEPRINT, all blueprints will be
 updated.
+
+If second branch or tag is specified, only blueprints that use second branch
+will be redirected to new branch.
 
 
 EXAMPLES:
 
   $0 blueprint.yaml 0.4.3  # Redirect blueprint.yaml to tag 0.4.3
-  $0 blueprint.yaml        # Redirect blueprint.yaml to branch tb-wip
-  $0 all            1.0.0  # Redirect all blueprints to tag 1.0.0
+  $0 blueprint.yaml        # Redirect blueprint.yaml to branch $DEFAULT_BRANCH
+  $0 all 1.0.0             # Redirect all blueprints to tag 1.0.0
+  $0 all 1.0.0 develop     # Redirect blueprints that use develop to 1.0.0
 
 EOF
 }
 
 function main ()
 {
-  local branch=$DEFAULT_BRANCH blueprints f replacement
+  local branch=$DEFAULT_BRANCH src='[^/]+' blueprints f replacement
 
   [[ $# -lt 1 ]] && usage && exit 0
 
   [[ $# -gt 1 ]] && branch="$2"
+  [[ $# -gt 2 ]] && src="$3"
   replacement=${REPLACEMENT/__BRANCH__/$branch}
+  needle=${NEEDLE/__SRC__/$src}
 
   if [[ "$1" == "all" ]]
   then
@@ -48,7 +54,7 @@ function main ()
 
   for f in $blueprints
   do
-    sed -i -re "s,$NEEDLE,$replacement," $f
+    sed -i -re "s,$needle,$replacement," $f
   done
 }
 
