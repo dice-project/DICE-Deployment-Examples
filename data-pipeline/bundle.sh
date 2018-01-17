07b101fcf9305a653f33cebd3e649c0999220843
Tadej Borovšak <tadej.borovsak@xlab.si>
2018-01-17 11:13:37 +0100

    Update bundle script for data pipeline example
    Since DICE TOSCA library 0.7.0, there is no separate import for
    different platforms, so asset bundling can be simplified.
    
    Change-Id: If38d82b0ed4dc2a6300b79664a67e996b0772525

--- data-pipeline/bundle.sh
+++ data-pipeline/bundle.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-for PLATFORM in openstack fco
-do
-	T=$(mktemp -d)
-	chmod 755 $T
-	cp blueprint-$PLATFORM.yaml $T/blueprint.yaml
-	cp -r resources scripts $T
-	tar -cvzf data-pipeline-$PLATFORM.tar.gz -C $(dirname $T) $(basename $T)
-	rm -rf $T
-	unset T
-done
+T=$(mktemp -d)
+chmod 755 $T
+cp blueprint.yaml $T/blueprint.yaml
+cp -r resources scripts $T
+tar -cvzf data-pipeline.tar.gz -C $(dirname $T) $(basename $T)
+rm -rf $T
+unset T
